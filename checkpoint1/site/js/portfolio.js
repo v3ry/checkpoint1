84db7495a2d874c6d961f96ee3c2b227225f05c0
v3ry <v3ryfr@gmail.com>
2022-06-01 13:29:36 +0200

    ajout changement de nom et de couleur du background

--- checkpoint1/site/js/portfolio.js
+++ checkpoint1/site/js/portfolio.js
@@ -3,10 +3,33 @@ let bProfilePicture = false;
 pictureProfile.addEventListener("click", ()=>{
     if (bProfilePicture === false){
         bProfilePicture = true;
-        pictureProfile.src = "image/avatar.svg"
+        pictureProfile.src = "image/avatar.svg";
     }else{
         bProfilePicture = false;
-        pictureProfile.src = "image/avatar-bis.png"
+        pictureProfile.src = "image/avatar-bis.png";
     }
     
+})
+
+//etape2
+const descriptionZone = document.querySelector(".description");
+const nameButton = document.createElement("button");
+const nameVal = document.querySelector("#name");
+
+nameButton.style.width="150px";
+nameButton.style.height="50px";
+nameButton.style.borderRadius = "11px";
+nameButton.style.borderColor = "white";
+nameButton.style.color = "white";
+nameButton.style.backgroundColor = "pink";
+nameButton.innerHTML = "Modify text and color";
+descriptionZone.appendChild(nameButton);
+
+
+nameButton.addEventListener("click", () =>{
+    let myColor = prompt("Qu'elle couleur voulez vous")
+    let val = prompt("Qu'elle est votre nom?")
+    descriptionZone.style.backgroundColor = myColor;
+    nameButton.style.backgroundColor = myColor;
+    nameVal.innerText = val
 })
