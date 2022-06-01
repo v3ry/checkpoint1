a16f1ebf67637ef6e81d7408aa81c4976cb5d3e2
v3ry <v3ryfr@gmail.com>
2022-06-01 18:10:07 +0200

    finalisation des étape bonus avec options back et front

--- checkpoint1/site/js/portfolio.js
+++ checkpoint1/site/js/portfolio.js
@@ -18,6 +18,8 @@ const nameVal = document.querySelector("#name");
 const pinkText = document.querySelectorAll(".pink-text");
 const pinkBg = document.querySelectorAll(".pink-bg");
 const linkColor = document.querySelectorAll(".menu a");
+const liList = document.querySelectorAll("#front-dev-tools li");
+const btnList = document.querySelector(".btnList");
 
 nameButton.style.width="150px";
 nameButton.style.height="50px";
@@ -36,16 +38,33 @@ nameButton.addEventListener("click", () =>{
     nameButton.style.backgroundColor = myColor;
     pinkText.forEach((myText)=> {
         myText.style.color = myColor;
-        // myText.innerHTML.color = "red";
     })
     pinkBg.forEach((myBg)=> {
         myBg.style.backgroundColor = myColor;
     })
-    linkColor.forEach((myColor)=> {
+    linkColor.forEach((linkColor)=> {
         console.log("test")
-        myColor.color = myColor
-        // myColor.style.color = myColor;
+        linkColor.style.color = myColor;
     })
-    // pinkBg.style.backgroundColor = myColor ;
     nameVal.innerText = val;
 })
+btnList.addEventListener("click", ()=>{
+    for(let i = 0; i < liList.length;i++){
+        let val1 = prompt(`Entrer le Dev Tools n° ${i}`)
+        liList[i].innerHTML = val1;
+    }
+})
+
+const backUl = document.querySelector("#backUl");
+const addBackend = document.querySelector("#addBackend");
+const btnBack = document.querySelector(".btnBack");
+
+
+let currentIndex = 0
+const myList = []
+btnBack.addEventListener("click",()=>{
+    let newList = document.createElement("li");
+    newList.innerText= addBackend.value
+    backUl.appendChild(newList);
+})
+
