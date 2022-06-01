dc1123110387e16047cf18302d3ef96be902282a
v3ry <v3ryfr@gmail.com>
2022-06-01 10:24:27 +0200

    correction d'un soucis avec git

---
+++ checkpoint1/algo/3/theaterSieges.js
@@ -0,0 +1,26 @@
+/*
+Un employé de théatre souhaiterait obtenir la liste de tous les sièges présents dans sa salle principale.
+Dans la salle les sièges sont répartis de la manière suivantes : 
+  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
+  - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
+
+Compléter la fonction theaterSieges de manière à ce qu'elle retourne une matrice 
+où chaque sous-tableau liste les positions des sieges d'une rangée.
+
+exemple :
+
+[
+  ['1-1', '1-2', '1-3', ..., '1-100'], 
+  ['2-1', '2-2', '2-3', ..., '2-100'],
+  ... 
+  ['26-1', '26-2', '26-3', ..., '26-100']
+] 
+*/
+
+function theaterSieges() {
+  // Your code here !
+}
+
+console.log(theaterSieges());
+
+module.exports = theaterSieges;
