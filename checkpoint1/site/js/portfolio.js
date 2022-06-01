b441103765b6ef6e24a564cad7998a6482dac13f
v3ry <v3ryfr@gmail.com>
2022-06-01 13:12:59 +0200

    correction changement photo profile

--- checkpoint1/site/js/portfolio.js
+++ checkpoint1/site/js/portfolio.js
@@ -1,5 +1,12 @@
 const pictureProfile = document.querySelector(".profilePicture")
-
+let bProfilePicture = false;
 pictureProfile.addEventListener("click", ()=>{
-    pictureProfile.src = "image/avatar.svg"
+    if (bProfilePicture === false){
+        bProfilePicture = true;
+        pictureProfile.src = "image/avatar.svg"
+    }else{
+        bProfilePicture = false;
+        pictureProfile.src = "image/avatar-bis.png"
+    }
+    
 })
